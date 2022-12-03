58759bb0e3b966898c2de82dd0661a427466ddd2
Sam Komarov <sam@skomarov.com>
2022-12-03 13:28:53 +0300

    use the spinner for the texts loading

--- src/lib/features/texts/delivery/components/TextScreen/TextScreenContainer.tsx
+++ src/lib/features/texts/delivery/components/TextScreen/TextScreenContainer.tsx
@@ -1,19 +1,22 @@
+import Spinner from "../../../../../core/delivery/components/Spinner/Spinner";
 import { uiDeps } from "../../../../../di";
 import { TextsBuilder, TextsProvider } from "../../../domain/state/TextsBloc";
 import { LoadedTextScreen } from "./TextScreen";
 
 export function TextScreenContainer({ }) {
     return (
-        <TextsProvider create={uiDeps.textsBloc}>
-            <TextsBuilder builder={(snapshot) => {
-                if (snapshot == null) {
-                    return <p>Loading...</p>;
-                }
-                else if (snapshot instanceof Error) {
-                    return <p>{snapshot.message}</p> // TODO: error handling
-                }
-                return <LoadedTextScreen state={snapshot} />
-            }} />
-        </TextsProvider>
+        <div id="textScreen">
+            <TextsProvider create={uiDeps.textsBloc}>
+                <TextsBuilder builder={(snapshot) => {
+                    if (snapshot == null) {
+                        return <Spinner />;
+                    }
+                    else if (snapshot instanceof Error) {
+                        return <p>{snapshot.message}</p> // TODO: error handling
+                    }
+                    return <LoadedTextScreen state={snapshot} />
+                }} />
+            </TextsProvider>
+        </div>
     );
 }

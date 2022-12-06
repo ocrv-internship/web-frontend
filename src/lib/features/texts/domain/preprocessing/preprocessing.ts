028261991ce82bceaafc2b5d81c086e5a35f72b3
Sam Komarov <sam@skomarov.com>
2022-12-06 10:43:14 +0300

    restrict adding accents only to vowels

--- src/lib/features/texts/domain/preprocessing/preprocessing.ts
+++ src/lib/features/texts/domain/preprocessing/preprocessing.ts
@@ -1,5 +1,5 @@
 function addAccents(text: string): string {
-    return text.replace(/\+(.)/gm, `$1\u0301`);
+    return text.replace(/\+([аоуыэеёиюяaeiouy])/gmi, `$1\u0301`);
 }
 
 function addBoldness(text: string): string {

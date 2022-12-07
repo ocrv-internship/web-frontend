a0731a76c4dc493d40049c0a8d16a991e0856f37
Sam Komarov <sam@skomarov.com>
2022-12-07 08:59:24 +0300

    change the progress tab to be horizontal

--- src/lib/features/texts/delivery/components/TextScreen/ProgressTab/ProgressTab.tsx
+++ src/lib/features/texts/delivery/components/TextScreen/ProgressTab/ProgressTab.tsx
@@ -7,10 +7,10 @@ export interface ProgressTabProps {
     fullDurationSec: number,
 }
 export function ProgressTab(props : ProgressTabProps) {
-    const duration = <h3>Общая длительность {formatDuration(props.fullDurationSec)}</h3>;
+    const duration = <h3>Общая длительность: {formatDuration(props.fullDurationSec)}</h3>;
     return (
         <section id="progress">
-            <h3>Закончено {props.completedCount} из {props.textsCount}</h3>
+            <h3>Закончено: {props.completedCount} из {props.textsCount}</h3>
             {props.fullDurationSec ? duration : <></>}
         </section>
     );

10059b50245f623baebaf0b7c8259e0753320543
Sam Komarov <sam@skomarov.com>
2022-12-12 19:40:27 +0300

    change the font buttons to be svg instead of text so they are centered properly

--- src/lib/features/texts/delivery/components/TextScreen/TextSection/TextSection.tsx
+++ src/lib/features/texts/delivery/components/TextScreen/TextSection/TextSection.tsx
@@ -17,8 +17,19 @@ export function TextSection({text}: {text: string}) {
                 <div id="fontSection">
                     <h2>{`Шрифт: ${fontSize}`}</h2>
                     <div id="fontButtons">
-                        <button onClick={decrement} className="simple">-</button>
-                        <button onClick={increment} className="simple">+</button>
+                        <button onClick={decrement} className="simple">
+                            <svg className="minusButton" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <line x1="5" y1="12" x2="19" y2="12"></line>
+                            </svg>
+                        </button>
+                        <button onClick={increment} className="simple">
+                            <svg className="plusButton"xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <line x1="12" y1="5" x2="12" y2="19"></line>
+                                <line x1="5" y1="12" x2="19" y2="12"></line>
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>

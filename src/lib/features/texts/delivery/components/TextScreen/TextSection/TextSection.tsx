8aee8b0b06c65c3c95d64a7cafaa69f60cd9d917
Sam Komarov <sam@skomarov.com>
2022-12-17 22:18:17 +0300

    refactor TextSection

--- src/lib/features/texts/delivery/components/TextScreen/TextSection/TextSection.tsx
+++ src/lib/features/texts/delivery/components/TextScreen/TextSection/TextSection.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import "./TextSection.css";
 
 const defaultFontSize = 18; 
+const minFontSize = 12; 
+const maxFontSize = 50; 
+const fontSizeStep = 2; 
 
 export function TextSection({text}: {text: string}) {
     const [fontSize, setFontSize] = useState(defaultFontSize);
-    const decrement = fontSize > 12 ? () => setFontSize(fontSize-2) : () => {};
-    const increment = fontSize < 50 ? () => setFontSize(fontSize+2) : () => {};
+    const decrement = fontSize > minFontSize ? 
+        () => setFontSize(fontSize-fontSizeStep) : () => {};
+    const increment = fontSize < maxFontSize ? 
+        () => setFontSize(fontSize+fontSizeStep) : () => {};
     const textInnerHTML = {
         __html: text, 
     };
@@ -16,21 +21,7 @@ export function TextSection({text}: {text: string}) {
                 <h2>Текст для записи</h2>
                 <div id="fontSection">
                     <h2>{`Шрифт: ${fontSize}`}</h2>
-                    <div id="fontButtons">
-                        <button onClick={decrement} className="simple">
-                            <svg className="minusButton" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth="1.25" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <line x1="5" y1="12" x2="19" y2="12"></line>
-                            </svg>
-                        </button>
-                        <button onClick={increment} className="simple">
-                            <svg className="plusButton"xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth="1.25" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <line x1="12" y1="5" x2="12" y2="19"></line>
-                                <line x1="5" y1="12" x2="19" y2="12"></line>
-                            </svg>
-                        </button>
-                    </div>
+                    <FontButtons increment={increment} decrement={decrement} />
                 </div>
             </div>
             <p 
@@ -39,5 +30,28 @@ export function TextSection({text}: {text: string}) {
                 dangerouslySetInnerHTML={textInnerHTML} 
             />
         </section>
+    );
+}
+
+function FontButtons({increment, decrement} : {
+    increment: () => void, 
+    decrement: () => void
+}) {
+    return (
+        <div id="fontButtons">
+            <button onClick={decrement} className="simple">
+                <svg className="minusButton" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth="1.25" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+            </button>
+            <button onClick={increment} className="simple">
+                <svg className="plusButton"xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth="1.25" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+            </button>
+        </div>
     );
 }
